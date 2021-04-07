da6fdf2e3457e5ed91b94115bf6df95f3b453bc0
jpapazian2000 <jpapazian@hashicorp.com>
2021-04-07 12:09:43 +0200

    all provisionned with variables

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "vmware_linux_vm" {
   source            = "github.com/nehrman/terraform-vmware-linux"
-  vmtemp            = "CentOS7-Template"
-  instances         = "1"
-  vmname            = "linux-vm"
-  vmrp              = "jeromeRP"
-  vlan              = "INTERNAL"
-  data_disk         = "false"
-  data_disk_size_gb = "10"
-  dc                = "MYVWORLD"
-  ds                = "vsanDatastore"
-  ipaddress         = ["192.168.0.40"]
-  ipv4submask       = "24"
-  vmgateway         = "192.168.0.1"
-  vmdns             = ["192.168.0.1"]
-  tag_category      = "web3"
-  tag               = "web3"
+  vmtemp            = var.vmtemp
+  instances         = var.instances
+  vmname            = var.vmname
+  vmrp              = var.vmrp
+  vlan              = var.vlan
+  data_disk         = var.data_disk
+  data_disk_size_gb = var.data_disk_size_gb
+  dc                = var.dc
+  ds                = var.ds
+  ipaddress         = var.ipaddress
+  ipv4submask       = var.ipv4submask
+  vmgateway         = var.vmgateway
+  vmdns             = var.vmdns
+  tag_category      = var.tag_category
+  tag               = var.tag
   storage_policy_name = var.storage_policy_name
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,105 @@ variable storage_policy_name {
     type = string
     default = "vSAN Default Storage Policy"
 }
+
+variable vmtemp {
+    type = string
+    description = "template to use for the VM"
+}
+variable instances {
+    type = string
+    description = "number of VM to launch"
+}
+
+variable vmname {
+    type = string
+    description = "name of the vm"
+}
+
+variable "vmnamesuffix" {
+  description = "vmname suffix after numbered index coming from instance variable"
+  default     = ""
+}
+
+variable vmrp {
+    type = string
+    description = "resource pool for the VM"
+}
+
+variable vlan {
+    type = string
+    description = "vlan name where the vm should be deployed"
+}
+
+variable "data_disk" {
+  type        = string
+  description = "Set to true to add a datadisk."
+  default     = "false"
+}
+
+variable "data_disk_size_gb" {
+  description = "Storage data disk size size"
+  default     = 20
+}
+
+variable "cpu_number" {
+  description = "number of CPU (core per CPU) for the VM"
+  default     = 2
+}
+
+variable "ram_size" {
+  description = "VM RAM size in megabytes"
+  default     = 4096
+}
+
+variable "ipv4submask" {
+  description = "ipv4 Subnet mask"
+  default     = 24
+}
+
+variable "ipaddress" {
+  description = "host(VM) IP address in list format, support more than one IP. Should correspond to number of instances"
+  type        = list(string)
+  default     = [""]
+}
+
+variable "vmdomain" {
+  description = "default VM domain for linux guest customization"
+  default     = "Development"
+}
+
+variable "dc" {
+  description = "Name of the datacenter you want to deploy the VM to"
+}
+
+variable "vmrp" {
+  description = "Cluster resource pool that VM will be deployed to. you use following to choose default pool in the cluster (esxi1) or (Cluster)/Resources"
+}
+
+variable "ds" {
+  description = "Datastore to deploy the VM."
+}
+
+variable "vmfolder" {
+  default = "Discovered virtual machine"
+}
+
+variable "vmgateway" {
+  description = "VM gateway to set during provisioning"
+  default     = ""
+}
+
+variable "vmdns" {
+  type    = list(string)
+  default = []
+}
+
+variable "tag_category" {
+  description = "Used to name a tags' category on vCenter"
+  default     = ""
+}
+
+variable "tag" {
+  description = "Used to name a tag on vCenter"
+  default     = ""
+}
