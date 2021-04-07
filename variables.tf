f317953b22b06486d0bc8fde821a7b5bc5013ab0
jpapazian2000 <jpapazian@hashicorp.com>
2021-04-07 12:10:47 +0200

    removed useless variable

--- variables.tf
+++ variables.tf
@@ -84,10 +84,6 @@ variable "dc" {
   description = "Name of the datacenter you want to deploy the VM to"
 }
 
-variable "vmrp" {
-  description = "Cluster resource pool that VM will be deployed to. you use following to choose default pool in the cluster (esxi1) or (Cluster)/Resources"
-}
-
 variable "ds" {
   description = "Datastore to deploy the VM."
 }
