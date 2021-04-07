a96f829f820f421f16b5c9864e46eafe13222b30
jpapazian2000 <jpapazian@hashicorp.com>
2021-04-07 11:14:01 +0200

    commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vmware_linux_vm" {
   source            = "github.com/nehrman/terraform-vmware-linux"
-  vmtemp            = "linuxVM"
+  vmtemp            = "CentOS-Template"
   instances         = "1"
   vmname            = "linux-vm"
   vmrp              = "jeromeRP"
